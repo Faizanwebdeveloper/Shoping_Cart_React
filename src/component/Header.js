385f1ca0b4fcedb2e7c2f96d85c522f6e44494c7
unknown <faizanwebdevloper@gmail.com>
2023-12-20 16:00:53 +0530

    Responsive Grid Coloum

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,13 +1,20 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import {
     Link
 } from "react-router-dom"
 import { State } from '../Context/Context';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
-    const { cart } = useContext(State);
-    return (
-        <section>
+  const { cart,category} = useContext(State);
+  const [sidecate,showsidecate] = useState(false);
+  const navigate = useNavigate();
+  const cat = (item) => {
+    navigate(`/category/${item}`);
+  }
+
+    return (<>
+       <section>
             <header>
                 <nav>
                     <div className='logo'>
@@ -17,10 +24,38 @@ const Header = () => {
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/product">Product</Link></li>
                         <li><Link to="/card">Card{cart.length > 0 && <sup>{cart.length}</sup>}</Link></li>
+                        <li onClick={()=>showsidecate(true)}>
+                         <a href='#'>Categories</a> 
+                        </li>
                     </ul>
                 </nav>
             </header>
         </section>
+       {/* Category SideBar */}
+           
+
+         {sidecate && 
+                 <div className='categories'>
+                 <div className='opac-layer'  onClick={()=>showsidecate(false)}></div>
+                 <div className='content'>
+                 <ul  onClick={()=>showsidecate(false)}>
+                   {category.map((item) => {
+                     return (
+                       <li onClick={() => cat(item)}>{item}</li>
+                     )
+                   })
+        
+                   }
+                 </ul>
+                 </div>
+             
+               </div>
+         
+  
+         }   
+          
+    </>
+     
     )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,8 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React from 'react';
 import Product from './Product';
-import { State } from '../Context/Context';
-import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-  const { category } = useContext(State);
-  const navigate = useNavigate();
-
-  const cat = (item) => {
-    navigate(`/category/${item}`);
-  }
-
-  return (
-    <>
-      <section>
-        <div className='categories'>
-          <ul>
-            {category.map((item) => {
-              return (
-                <li onClick={() => cat(item)}>{item}</li>
-              )
-            })
-
-            }
-          </ul>
-        </div>
-      </section>
-      <Product />
-    </>
-  )
+  return <Product />
 }
 
 export default Home
